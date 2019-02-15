ae4317b3cec88be3e42bd321b8546d138a9def9d
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-02-15 13:18:38 -0500

    added problem 2

---
+++ Problem2/problem2.c
@@ -0,0 +1,22 @@
+#include "problem2.h"
+
+/* * * * * * * * * * * * * * * * * * * * * * *
+* Costume department initialization methods
+* * * * * * * * * * * * * * * * * * * * *  * */
+
+
+
+/* * * * * * * * * * * * * * * * * * * * * * *
+*	Main()
+* * * * * * * * * * * * * * * * * * * * * * * */
+
+int main(int argc, char **argv) {
+
+	srand48(SEED_VAL);
+
+
+
+
+	return 0;
+
+}
---
+++ Problem2/problem2.h
@@ -0,0 +1,14 @@
+#ifndef PROBLEM1_H
+#define PROBLEM1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <math.h>
+#include <limits.h>
+
+#define SEED_VAL (100)
+
+#endif
